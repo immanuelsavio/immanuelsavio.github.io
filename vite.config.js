fe02a481689e5c2c0c3427b2ca3cf9cbb87d49ce
_Algorithm_Kid98 <30970161+immanuelsavio@users.noreply.github.com>
2025-11-21 09:37:12 -0600

    fix: use relative base path and enhance debug logging

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',          // load everything from /
+  base: './',          // load everything from relative path
   plugins: [react()],
 })
